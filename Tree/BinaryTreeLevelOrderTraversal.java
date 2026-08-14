d56a4258f5da3598fa8b897544dcc375bf3d2512
Ajay Patil <aj9411979585@gmail.com>
2026-08-14 16:27:04 +0530

    dsa(tree): solve Binary Tree Level Order Traversal (LeetCode 102)

---
+++ Tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,91 @@
+// Problem: Binary Tree Level Order Traversal
+// Difficulty: Medium
+// Topic: Tree
+// LeetCode Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (q.size() > 0) {
+
+            List<Integer> list = new ArrayList<>();
+
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode front = q.remove();
+
+                list.add(front.val);
+
+                if (front.left != null) {
+                    q.add(front.left);
+                }
+
+                if (front.right != null) {
+                    q.add(front.right);
+                }
+            }
+
+            ans.add(list);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        BinaryTreeLevelOrderTraversal solution =
+                new BinaryTreeLevelOrderTraversal();
+
+        // Test 1: [3,9,20,null,null,15,7]
+        TreeNode test1 = new TreeNode(3);
+        test1.left = new TreeNode(9);
+        test1.right = new TreeNode(20);
+        test1.right.left = new TreeNode(15);
+        test1.right.right = new TreeNode(7);
+
+        // Test 2: [1]
+        TreeNode test2 = new TreeNode(1);
+
+        // Test 3: []
+        TreeNode test3 = null;
+
+        System.out.println("Test 1: " + solution.levelOrder(test1));
+        System.out.println("Test 2: " + solution.levelOrder(test2));
+        System.out.println("Test 3: " + solution.levelOrder(test3));
+    }
+}
